cf055922901e8468c3c6c2e6ed8b88c3f95fabbe
Ilya Fadeev <fadeev.ilya@gmail.com>
2016-10-11 18:48:53 -0400

    test case for fixture store

--- test/test-crud.js
+++ test/test-crud.js
@@ -7,39 +7,46 @@ var fixtureSocket = require('./can-fixture-socket').Server;
  * - on deleted send ACK with {success: true} and emit deleted event with the removed message id.
  */
 
-// Mock server:
-const mockServer = new fixtureSocket.Server();
-mockServer.on('connection', function(socket){
-	mockServer.on('message created', function(data, fn){
-		data.id = 1;
+var mockServer;
 
-		// send ack on the received event:
-		fn(data);
+QUnit.module('Low level API', {
+	beforeEach: function(){
+		// Mock server:
+		mockServer = new fixtureSocket.Server();
+		mockServer.on('connection', function(socket){
+			mockServer.on('message created', function(data, fn){
+				data.id = 1;
 
-		mockServer.emit('message created', data);
-	});
+				// send ack on the received event:
+				fn(data);
 
-	mockServer.on('message updated', function(data, fn){
-		// send ack on the received event:
-		fn(data);
+				mockServer.emit('message created', data);
+			});
 
-		mockServer.emit('message updated', data);
-	});
+			mockServer.on('message updated', function(data, fn){
+				// send ack on the received event:
+				fn(data);
 
-	mockServer.on('message deleted', function(data, fn){
-		// send ack on the received event:
-		fn({success: true});
+				mockServer.emit('message updated', data);
+			});
 
-		mockServer.emit('message updated', {id: data.id});
-	});
+			mockServer.on('message deleted', function(data, fn){
+				// send ack on the received event:
+				fn({success: true});
+
+				mockServer.emit('message updated', {id: data.id});
+			});
+		});
+	},
+	afterEach: function(){
+		mockServer.reset();
+	}
 });
-
-QUnit.module('Test CRUD resource');
 
 QUnit.test('Message CRUD', function(){
 	QUnit.expects(5);
 
-	var socket = io('/message');
+	var socket = io('/messages');
 
 	socket.on('connect', function(){
 		socket.emit('create message', {title: 'A new message'}, function(data){
@@ -65,5 +72,30 @@ QUnit.test('Message CRUD', function(){
 
 	socket.on('message deleted', function(data) {
 		QUnit.deepEqual(data, {title: 'An updated message', id: 1});
+	});
+});
+
+QUnit.module('Fixture store', {
+	before: function(){
+		mockServer = new fixtureSocket.ServerStore({
+			messages: [
+				{id: 1, title: 'One'},
+				{id: 2, title: 'Two'}
+			]
+		});
+	}
+});
+
+QUnit.test('Test fixture store', function(){
+
+	var socket = io('/messages');
+	
+	socket.on('connect', function(){
+		socket.emit('messages find', {}, function(data){
+			QUnit.equal(data.length, 2);
+		});
+		socket.emit('create message', {title: 'A new message'}, function(data){
+			QUnit.deepEqual(data, {id: 1});
+		});
 	});
 });
