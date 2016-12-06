e1ec52a4e32cfbedd81c31a951342a701dd39e80
Ilya Fadeev <fadeev.ilya@gmail.com>
2016-12-06 18:16:15 -0500

    0.5.10

---
+++ dist/amd/src/store.js
@@ -0,0 +1,35 @@
+/*can-fixture-socket@0.5.9#src/store*/
+define(function (require, exports, module) {
+    var extractResponse = require('can-fixture/core').extractResponse;
+    function requestHandlerToListener(method) {
+        return function (query, fn) {
+            var req = { data: query };
+            var res = function () {
+                var response = extractResponse.apply(null, arguments);
+                if (response[0] === 200) {
+                    fn(null, response[1]);
+                } else {
+                    fn(response[1]);
+                }
+            };
+            method(req, res);
+        };
+    }
+    function storeToListeners(fixtureStore) {
+        var methods = [
+            'getListData',
+            'getData',
+            'updateData',
+            'createData',
+            'destroyData'
+        ];
+        return methods.reduce(function (listeners, method) {
+            listeners[method] = requestHandlerToListener(fixtureStore[method]);
+            return listeners;
+        }, {});
+    }
+    module.exports = {
+        requestHandlerToListener: requestHandlerToListener,
+        storeToListeners: storeToListeners
+    };
+});
