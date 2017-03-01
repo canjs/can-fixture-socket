4178cd07ef186be86876bf22d0ca0abaf38d69f8
Marshall Thompson <marshall@creativeideal.net>
2017-03-01 10:20:42 -0700

    0.6.2

---
+++ dist/cjs/can-fixture-socket.js
@@ -0,0 +1,8 @@
+/*can-fixture-socket@0.6.1#can-fixture-socket*/
+var fixtureSocket = require('./src/index.js');
+var fixtureStore = require('./src/store.js');
+module.exports = {
+    Server: fixtureSocket.Server,
+    requestHandlerToListener: fixtureStore.requestHandlerToListener,
+    storeToListeners: fixtureStore.storeToListeners
+};
