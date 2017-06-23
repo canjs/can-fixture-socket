8f9c37c5532b5f46fc5af2c484e26297d0689d19
Kevin Phillips <kevin@bitovi.com>
2017-06-22 21:44:27 -0500

    0.7.0

---
+++ dist/cjs/can-fixture-socket.js
@@ -0,0 +1,8 @@
+/*can-fixture-socket@0.7.0-pre.1#can-fixture-socket*/
+var fixtureSocket = require('./src/index.js');
+var fixtureStore = require('./src/store.js');
+module.exports = {
+    Server: fixtureSocket.Server,
+    requestHandlerToListener: fixtureStore.requestHandlerToListener,
+    storeToListeners: fixtureStore.storeToListeners
+};
---
+++ dist/cjs/src/index.js
@@ -0,0 +1,142 @@
+/*can-fixture-socket@0.7.0-pre.1#src/index*/
+var subscribeFeathersStoreToServer = require('./feathers-client.js').subscribeFeathersStoreToServer;
+var MockedServer = function (io) {
+    this.io = io;
+    this.events = {};
+    this.subscribers = {};
+    resetManagerCache(io.managers);
+    this.origs = mockManager(io.Manager.prototype, this);
+};
+MockedServer.prototype.on = function (event, cb) {
+    var self = this;
+    var events = {};
+    if (typeof event === 'string') {
+        events[event] = cb;
+    }
+    if (typeof event === 'object') {
+        events = event;
+    }
+    Object.keys(events).forEach(function (name) {
+        sub(self.events, name, events[name]);
+    });
+};
+MockedServer.prototype.emit = function (event) {
+    var dataArgs = Array.prototype.slice.call(arguments, 1);
+    pub(this.subscribers, event, dataArgs);
+};
+MockedServer.prototype.onFeathersService = function (serviceName, fixtureStore, options) {
+    subscribeFeathersStoreToServer(serviceName, fixtureStore, this, options);
+};
+MockedServer.prototype.restore = function () {
+    restoreManager(this.io.Manager.prototype, this.origs);
+    resetManagerCache(this.io.managers);
+};
+var MockedSocket = function (server) {
+    this._server = server;
+    this.io = { engine: this };
+};
+MockedSocket.prototype = {
+    on: function (event, cb) {
+        debug('MockedSocket.on ... ' + event);
+        sub(this._server.subscribers, event, cb);
+    },
+    emit: function (event) {
+        var dataArgs = Array.prototype.slice.call(arguments, 1);
+        debug('MockedSocket.emit ...' + event);
+        pub(this._server.events, event, dataArgs);
+    },
+    once: function () {
+        debug('MockedSocket.once ...');
+    },
+    off: function (event, cb) {
+        debug('MockedSocket.off ... ' + event);
+        unsub(this._server.subscribers, event, cb);
+    },
+    open: function () {
+        return this.connect();
+    },
+    connect: function () {
+        this.connected = true;
+        this.disconnected = false;
+    },
+    close: function () {
+        return this.disconnect();
+    },
+    disconnect: function () {
+        this.connected = false;
+        this.disconnected = true;
+    }
+};
+function pub(pubsub, event, dataArgs) {
+    debug(' >>> pub ' + event);
+    var subscribers = pubsub[event] || [];
+    subscribers.forEach(function (subscriber) {
+        subscriber.apply(null, dataArgs);
+    });
+}
+function sub(pubsub, event, cb) {
+    debug(' <<< sub ' + event);
+    if (!pubsub[event]) {
+        pubsub[event] = [];
+    }
+    pubsub[event].push(cb);
+}
+function unsub(pubsub, event, cb) {
+    debug(' <<< unsub ' + event);
+    pubsub[event].forEach(function (registeredCb, index) {
+        if (registeredCb === cb) {
+            pubsub[event].splice(index, 1);
+        }
+    });
+}
+function mockManager(managerProto, server) {
+    var methods = [
+        'open',
+        'socket'
+    ];
+    var origs = methods.map(function (name) {
+        return {
+            name: name,
+            method: managerProto[name]
+        };
+    });
+    managerProto.open = managerProto.connect = function () {
+        debug('MockedManager.prototype.open or connect ... arguments:', arguments);
+        setTimeout(function () {
+            pub(server.subscribers, 'connect');
+            pub(server.events, 'connection');
+        }, 0);
+    };
+    managerProto.socket = function () {
+        debug('MockedManager.prototype.socket ...');
+        var socket = new MockedSocket(server);
+        socket.connected = true;
+        socket.disconnected = false;
+        return socket;
+    };
+    return origs;
+}
+function restoreManager(managerProto, origs) {
+    debug('Restore.');
+    origs.forEach(function (orig) {
+        managerProto[orig.name] = orig.method;
+    });
+}
+function resetManagerCache(cache) {
+    for (var i in cache) {
+        if (cache.hasOwnProperty(i)) {
+            delete cache[i];
+        }
+    }
+}
+var _DEBUG = false;
+function debug(msg, obj) {
+    if (_DEBUG) {
+        console.log.apply(console, arguments);
+    }
+}
+module.exports = {
+    Server: MockedServer,
+    mockSocketManager: mockManager,
+    restoreManager: restoreManager
+};
