969d4af5acd542ebd88382532cd7924a16ef602f
Kevin Phillips <kevin@bitovi.com>
2018-01-29 17:04:32 -0600

    1.0.0

---
+++ dist/cjs/src/store.js
@@ -0,0 +1,33 @@
+/*can-fixture-socket@1.0.0-pre.1#src/store*/
+var extractResponse = require('can-fixture/core').extractResponse;
+function requestHandlerToListener(method) {
+    return function (query, fn) {
+        var req = { data: query };
+        var res = function () {
+            var response = extractResponse.apply(null, arguments);
+            if (response[0] === 200) {
+                fn(null, response[1]);
+            } else {
+                fn(response[1]);
+            }
+        };
+        method(req, res);
+    };
+}
+function storeToListeners(fixtureStore) {
+    var methods = [
+        'getListData',
+        'getData',
+        'updateData',
+        'createData',
+        'destroyData'
+    ];
+    return methods.reduce(function (listeners, method) {
+        listeners[method] = requestHandlerToListener(fixtureStore[method]);
+        return listeners;
+    }, {});
+}
+module.exports = {
+    requestHandlerToListener: requestHandlerToListener,
+    storeToListeners: storeToListeners
+};
